c5131c217d7705166a5a67213cd4541a98a00358
wangjun <444055828@qq.com>
2023-12-05 16:28:52 +0800

    v0.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,73 @@
 pub mod error;
 pub mod utils;
 
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::ops::Deref;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Array<T>(Vec<T>);
+#[derive(Debug, Clone)]
+pub struct Array<T>(pub Vec<T>);
 
-pub fn deserialize_array<'de, T, D>(deserializer: D) -> Result<Array<T>, D::Error>
-where
-    for<'d> T: Serialize + Deserialize<'d>,
-    D: serde::Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    let v = serde_json::from_str::<Vec<T>>(&s).map_err(serde::de::Error::custom)?;
-    Ok(Array(v))
-}
+impl<T> Deref for Array<T> {
+    type Target = Vec<T>;
 
-pub fn serialize_array<T, S>(array: &Array<T>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    T: Serialize,
-    S: serde::Serializer,
-{
-    let s = serde_json::to_string(&array.0).map_err(serde::ser::Error::custom)?;
-    serializer.serialize_str(&s)
-}
-
-pub fn deserialize_option_array<'de, T, D>(deserializer: D) -> Result<Option<Array<T>>, D::Error>
-where
-    for<'d> T: Serialize + Deserialize<'d>,
-    D: serde::Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    if s.is_empty() {
-        return Ok(None);
-    }
-    let v = serde_json::from_str::<Vec<T>>(&s).map_err(serde::de::Error::custom)?;
-    Ok(Some(Array(v)))
-}
-
-pub fn serialize_option_array<T, S>(
-    array: &Option<Array<T>>,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    T: Serialize,
-    S: serde::Serializer,
-{
-    match array {
-        Some(array) => serialize_array(array, serializer),
-        None => serializer.serialize_none(),
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct MyStruct {
-    #[serde(
-        deserialize_with = "deserialize_array",
-        serialize_with = "serialize_array"
-    )]
-    ids: Array<String>,
+impl<'de, T> Deserialize<'de> for Array<T>
+where
+    T: DeserializeOwned,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s: String = Deserialize::deserialize(deserializer)?;
+        let v = serde_json::from_str::<Vec<T>>(&s).map_err(serde::de::Error::custom)?;
+        Ok(Array(v))
+    }
 }
 
-fn main() {
-    let s = r#"{"ids": "[\"1\", \"2\", \"3\", \"4\", \"5\"]"}"#;
-    println!("{:?}", serde_json::from_str::<MyStruct>(s).unwrap());
-    println!(
-        "{:}",
-        serde_json::to_string(&MyStruct {
-            ids: Array(vec![
-                "1".to_string(),
-                "2".to_string(),
-                "3".to_string(),
-                "4".to_string(),
-                "5".to_string()
-            ])
-        })
-        .unwrap()
-    );
+impl<T> Serialize for Array<T>
+where
+    T: Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let s = serde_json::to_string(&self.0).map_err(serde::ser::Error::custom)?;
+        serializer.serialize_str(&s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct MyStruct {
+        ids: Array<String>,
+    }
+
+    #[test]
+    fn test_serde() {
+        let s = r#"{"ids": "[\"1\", \"2\", \"3\", \"4\", \"5\"]"}"#;
+        println!("{:?}", serde_json::from_str::<MyStruct>(s).unwrap());
+        println!(
+            "{:}",
+            serde_json::to_string(&MyStruct {
+                ids: Array(vec![
+                    "1".to_string(),
+                    "2".to_string(),
+                    "3".to_string(),
+                    "4".to_string(),
+                    "5".to_string()
+                ])
+            })
+            .unwrap()
+        );
+    }
 }
