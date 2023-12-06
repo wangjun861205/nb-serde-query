c157489da81a368a674b2ff2729a6a63418fc834
wangjun <444055828@qq.com>
2023-12-06 23:51:22 +0800

    add deserializer

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod utils;
 use crate::error::Error;
 use base64::prelude::*;
 use serde::{
-    de::DeserializeOwned,
+    de::{DeserializeOwned, MapAccess},
     ser::{
         SerializeMap, SerializeSeq, SerializeStruct, SerializeStructVariant, SerializeTuple,
         SerializeTupleStruct, SerializeTupleVariant,
@@ -420,6 +420,332 @@ impl serde::Serializer for &mut Serializer {
     }
 }
 
+use serde::de::Visitor;
+use std::collections::HashMap;
+pub struct Deserializer {
+    m: HashMap<String, String>,
+    curr_key: Option<String>,
+    curr_val: Option<String>,
+    fields: Vec<String>,
+}
+
+impl Deserializer {
+    pub fn try_from_str(s: &str) -> Result<Self, Error> {
+        let m = s
+            .split('&')
+            .map(|p| p.split('='))
+            .try_fold(HashMap::new(), |mut m, mut p| {
+                let key = p.next().ok_or(Error::new("invalid key", None))?;
+                let val = p.next().ok_or(Error::new("invalid value", None))?;
+                m.insert(key.to_string(), val.to_string());
+                Ok(m)
+            })?;
+        Ok(Self {
+            m,
+            curr_key: None,
+            curr_val: None,
+            fields: Vec::new(),
+        })
+    }
+}
+
+impl<'de> MapAccess<'de> for Deserializer {
+    type Error = Error;
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error>
+    where
+        K: serde::de::DeserializeSeed<'de>,
+    {
+        // if let Some(k) = self.m.keys().next() {
+        //     self.curr_key = Some(k.clone());
+        //     return seed.deserialize(self).map(Some);
+        // }
+        // Ok(None)
+        if let Some(k) = self.fields.pop() {
+            self.curr_key = Some(k);
+            return seed.deserialize(self).map(Some);
+        }
+        Ok(None)
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::DeserializeSeed<'de>,
+    {
+        let k = self.curr_key.take().ok_or(Error::new("no key", None))?;
+        if let Some(v) = self.m.remove(&k) {
+            self.curr_val = Some(v);
+            return seed.deserialize(self);
+        }
+        seed.deserialize(self)
+    }
+}
+
+impl<'de> serde::Deserializer<'de> for &mut Deserializer {
+    type Error = Error;
+
+    fn deserialize_struct<V>(
+        self,
+        _name: &'static str,
+        fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        let mut next_deserializer = Deserializer {
+            m: self.m.clone(),
+            curr_key: None,
+            curr_val: None,
+            fields: fields.iter().map(|s| s.to_string()).collect(),
+        };
+        next_deserializer.deserialize_map(visitor)
+    }
+
+    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_map(self)
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_str(&self.curr_key.clone().ok_or(Error::new("no key", None))?)
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_bool(
+            self.curr_val
+                .take()
+                .ok_or(Error::new("no bool value", None))?
+                .parse()
+                .map_err(|e| Error::new("invalid bool literial", Some(Box::new(e))))?,
+        )
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        visitor.visit_i32(
+            self.curr_val
+                .take()
+                .ok_or(Error::new("no i32 value", None))?
+                .parse()
+                .map_err(|e| Error::new("invalid i32 literial", Some(Box::new(e))))?,
+        )
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_string(
+            self.curr_val
+                .take()
+                .ok_or(Error::new("no string value", None))?,
+        )
+    }
+
+    fn deserialize_any<V>(self, _visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i8<V>(self, _visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i16<V>(self, _visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_enum<V>(
+        self,
+        name: &'static str,
+        variants: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i128<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: serde::de::Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_newtype_struct<V>(
+        self,
+        name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_tuple_struct<V>(
+        self,
+        name: &'static str,
+        len: usize,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u128<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_unit_struct<V>(
+        self,
+        name: &'static str,
+        visitor: V,
+    ) -> Result<V::Value, Self::Error>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -491,5 +817,24 @@ mod tests {
             c: None,
         };
         println!("{}", to_string(&s).unwrap());
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    struct De {
+        name: String,
+        age: i32,
+        // #[serde(flatten)]
+        pagination: Pagination,
+    }
+
+    #[test]
+    fn test_deserializer() {
+        let mut deserializer =
+            Deserializer::try_from_str("age=37&name=test&offset=0&limit=10").unwrap();
+        let de = De::deserialize(&mut deserializer).unwrap();
+        assert!(de.name == "test");
+        assert!(de.age == 37);
+        assert!(de.pagination.limit == 10);
+        assert!(de.pagination.offset == 0);
     }
 }
